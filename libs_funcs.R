bc9d973c4c2abbe7a1f7d47a1e8fbf4c96c666c2
Seymurhh <55957961+Seymurhh@users.noreply.github.com>
2023-12-02 19:11:45 -0500

    Libraries and functions have been moved into the "libs_funcs.R" function and source("libs_funcs.R") has been added at the beginning of the RMD file to load it. If you will need to add a new library, make sure to add it inside the libs_funcs.R function.

---
+++ libs_funcs.R
@@ -0,0 +1,72 @@
+
+# Libraries
+
+library(pheatmap)
+library(scales)
+library(readr)
+library(ggmap)
+library(ggpubr)
+library(hrbrthemes)
+library(ggcorrplot)
+library(GGally)
+library(car)
+library(caret)
+library(pwr)
+library(EnvStats)
+library(tidyverse)
+library(lmtest)
+library(knitr)
+library(fastDummies)
+library(rmarkdown)
+library(markdown)
+library(tidyr)
+library(MASS)
+library(ggplot2)
+library(lattice)
+library(dplyr)
+library(olsrr)
+library(nnet)
+library(neuralnet)
+library(corrplot)
+library(gridExtra)
+library(fpc)
+
+# Functions 
+
+# Function to load or download libraries
+
+loadLib = function(libName)
+{ 
+  if(require(libName, character.only=TRUE))
+  {
+    ##cat(libName, "loaded properly\n")
+  } else {
+    cat("Installing", libName, "\n")
+    install.packages(libName)
+    if(require(libName, character.only=TRUE))
+    {
+      cat(libName, "loaded properly\n")
+    } else {
+      stop(c(libName, "not properly installed\n"))
+    }
+  }
+}
+
+# Helper to display regression function with n coefficients
+
+dispRegFunc <- function(reg) {
+  coefs <- reg$coefficients
+  b0 = coefs[1]
+  n <- length(coefs)
+  my_formula <- paste0("Y = ", round(b0, digits = 6))
+  for (i in 2:n) {
+    my_formula <- paste0(my_formula, " + ", round(coefs[i],6), names(coefs)[i])
+  }
+  my_formula
+}
+
+
+  
+
+
+  
